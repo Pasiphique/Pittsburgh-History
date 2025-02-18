97552b4ffd75bf8cb3057bf7720aa5aadc128532
Pasiphique <pasiphiqueoswald@gmail.com>
2025-02-18 13:09:42 -0500

    fixed routing basename

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import PittsburghTimeline from './Pages/PittsburghTimeline.jsx'
 import { Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
 import './styles/App.css'
+
+
 const App = () => {
 
   const [menuActive, setMenuActive] = useState(false);
@@ -14,40 +16,44 @@ const App = () => {
     setMenuActive(!menuActive);
   };
 
-    return(
-     <div>
-        <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">Pittsburgh</Link>
-          <ul className={`navbar-menu ${menuActive ? 'active' : ''}`}>
-            <li>
-              <Link to="/" className="navbar-link">Home</Link>
-            </li>
-            <li>
-              <Link to="/map" className="navbar-link">Pittsburgh Map</Link>
-            </li>
-            <li>
-              <Link to="/timeline" className="navbar-link">Pittsburgh Timeline</Link>
-            </li>
-            <li>
-              <Link to='facts' className='navbar-link'>Fun Facts</Link>
-            </li>
-          </ul>
-          <div className="navbar-toggle"  onClick={toggleMenu}>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
-        </div>
-      </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/timeline" element={<PittsburghTimeline />} />
-          <Route path='/facts' element={<FunFacts />} />
-        </Routes>
-     </div>
-    )
-}
-export default App;
+
+    return (
+        <div>
+          <nav className="navbar">
+            <div className="navbar-container">
+              <Link to="/" className="navbar-logo">Pittsburgh</Link>
+              <ul className={`navbar-menu ${menuActive ? 'active' : ''}`}>
+                <li>
+                  <Link to="/" className="navbar-link">Home</Link>
+                </li>
+                <li>
+                  <Link to="/map" className="navbar-link">Pittsburgh Map</Link>
+                </li>
+                <li>
+                  <Link to="/timeline" className="navbar-link">Pittsburgh Timeline</Link>
+                </li>
+                <li>
+                  <Link to="/facts" className="navbar-link">Fun Facts</Link>
+                </li>
+              </ul>
+              <div className="navbar-toggle" onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </div>
+            </div>
+          </nav>
+  
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/map" element={<Map />} />
+            <Route path="/timeline" element={<PittsburghTimeline />} />
+            <Route path="/facts" element={<FunFacts />} />
+          </Routes>
+        </div>
+    );
+  }
+  
+  export default App;
+  
